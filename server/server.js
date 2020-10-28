90002aa7ee86ecb1eb38514170b0300f2e840fea
Nicolas RAZANAKOLONA <robelnicl@gmail.com>
2020-10-28 14:20:08 +0100

    add connect and disconnect event listener

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 
-var app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("Un nouvel utilisateur connécté");
+
+    socket.on('disconnect', (socket) => {
+        console.log("Utilisateur déconnécté");
+    });
+});
+
+server.listen(port, () => {
     console.log(`serveur lancé sur le port ${port}`)
-})
+});
